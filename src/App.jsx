ad60ac8d2f531bf63449f8a1ed9307ac0b3c3d33
Ahmed Hawas <ahmedhawas600@gmail.com>
2025-10-26 08:51:28 +0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px', fontFamily: 'sans-serif' }}>
+      <h1>🚀 CARVerID</h1>
+      <p>Your site is running on Vercel.</p>
+      <p>Developed by Ahmed Hawas</p>
+    </div>
+  );
+}
