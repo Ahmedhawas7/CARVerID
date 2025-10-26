80ecca8d3fc0507fd3ec0a07e87da8772772aefd
Ahmed Hawas <ahmedhawas600@gmail.com>
2025-10-26 11:07:22 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -20,12 +20,12 @@ export default function App() {
     <WagmiConfig config={wagmiConfig}>
       <RainbowKitProvider chains={chains}>
         <div className="min-h-screen flex flex-col justify-center items-center p-4">
-          <div className="bg-gray-300 shadow-lg rounded-xl p-8 max-w-md w-full flex flex-col items-center">
+          <div className="bg-black/50 backdrop-blur-md shadow-lg rounded-xl p-8 max-w-md w-full flex flex-col items-center">
             <h1 className="text-3xl font-bold mb-1 text-white">
               CARVerID
             </h1>
-            <span className="text-sm text-gray-700 mb-6">Demo</span>
-            <p className="mb-6 text-gray-800 text-center">
+            <span className="text-sm text-white mb-6">Demo</span>
+            <p className="mb-6 text-white text-center">
               Login with your wallet to experience CARVerID system.
             </p>
             <ConnectButton className="mb-4"/>
