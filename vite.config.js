310f165254b89dab3e3599b03cfbe29c7bae58f9
Ahmed Hawas <ahmedhawas600@gmail.com>
2025-10-26 11:28:26 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  root: '.',  // هذا يحدد أن الجذر هو المكان الذي فيه index.html
   plugins: [react()],
+  server: {
+    port: 5173,
+  },
   build: {
     outDir: 'dist',
   },
